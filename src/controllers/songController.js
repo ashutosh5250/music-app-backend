439f0f68753180e095f7dd87589b2b8f68fbb3cf
Ashutosh kushire <ashutoshkushire97@gmail.com>
2024-08-22 08:06:48 +0530

    first commit

--- src/controllers/songController.js
+++ src/controllers/songController.js
@@ -1,13 +1,24 @@
 const Song = require("../models/songsModel");
 
-const getSongs = async(req,res)=>{
-   const songs = await Song.find();
-	return res.status(200).send(songs);
-}
+const getSongs = async (req, res) => {
+  try {
+    const songs = await Song.find();
+    return res.status(200).send(songs);
+  } catch (error) {
+    console.error("Error fetching songs:", error);
+    return res.status(500).send({ message: "Failed to fetch songs", error: error.message });
+  }
+};
 
-const addSongs = async(req,res)=>{
-	const song = await Song(req.body).save();
-	return res.status(201).send({ data: song, message: "Song created successfully" });
-}
+const addSongs = async (req, res) => {
+  try {
+    const song = new Song(req.body);
+    await song.save();
+    return res.status(201).send({ data: song, message: "Song created successfully" });
+  } catch (error) {
+    console.error("Error adding song:", error);
+    return res.status(500).send({ message: "Failed to create song", error: error.message });
+  }
+};
 
-module.exports = {getSongs,addSongs}
+module.exports = { getSongs, addSongs };
