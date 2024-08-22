2ed76ee82fd931cd297a7a88dbecfb758fd5c3fa
Ashutosh kushire <ashutoshkushire97@gmail.com>
2024-08-22 20:05:42 +0530

    first commit

--- src/middelwares/verifyAuthToken.js
+++ src/middelwares/verifyAuthToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require("../models/usersModel");
 require("dotenv").config();
-const auth = async (req, res, next) => {
+const authToken = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '');
         const decoded = jwt.verify(token, process.env.PRIVATEKEY); 
@@ -17,7 +17,5 @@ const auth = async (req, res, next) => {
         res.status(401).send({ error: 'Please authenticate.' });
     }
 };
-
-module.exports = auth;
 
 module.exports = authToken;
