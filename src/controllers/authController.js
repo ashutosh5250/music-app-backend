ffa8d7888f5b3081d1ed42b31ddf96e71b48180b
Ashutosh kushire <ashutoshkushire97@gmail.com>
2024-08-22 07:51:26 +0530

    first commit

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,36 +4,52 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const signIn = async (req, res) => {
-   const { email, password } = req.body;
-   const user = await User.findOne({ email });
-   if (!user) {
-     return res.status(400).send({ message: "invalid email or password" });
-   }
-   const validPassword = await bcrypt.compare(password, user.password);
-   if (!validPassword) {
-     return res.status(400).send({ message: "invalid email or password" });
-   }
-   const token = jwt.sign({userName:user.userName,_id:user._id }, process.env.PRIVATEKEY, {
-     expiresIn: "10d",
-   });
-   return res.status(200).send({ data: token, message: "sign in sucessfully." });
- };
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
+    if (!user) {
+      return res.status(400).send({ message: "Invalid email or password" });
+    }
 
- const signUp = async (req, res) => {
-   const user = await User.findOne({email:req.body.email});
-   if (user) {
-     return res
-       .status(403)
-       .send({ message: "User with given email already Exist!" });
-   }
-   const salt = await bcrypt.genSalt(10);
-   const hashedPassword = await bcrypt.hash(req.body.password, salt);
-   let newUser = await new User({
-     ...req.body,
-     password: hashedPassword,
-   }).save();
-   res.status(200).send({message: "Account created sucessfully!" });
- }
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return res.status(400).send({ message: "Invalid email or password" });
+    }
 
- module.exports = {signIn,signUp}
+    const token = jwt.sign(
+      { userName: user.userName, _id: user._id },
+      process.env.PRIVATEKEY,
+      { expiresIn: "10d" }
+    );
+
+    return res.status(200).send({ token, userName: user.userName, message: "Sign in successfully." });
+  } catch (error) {
+    console.error("SignIn Error: ", error);
+    return res.status(500).send({ message: "Internal Server Error" });
+  }
+};
+
+const signUp = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(403).send({ message: "User with given email already exists!" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    let newUser = await new User({
+      ...req.body,
+      password: hashedPassword,
+    }).save();
+
+    return res.status(200).send({ message: "Account created successfully!" });
+  } catch (error) {
+    console.error("SignUp Error: ", error);
+    return res.status(500).send({ message: "Internal Server Error" });
+  }
+};
+
+module.exports = { signIn, signUp };
